fbb5ebe52a11e800dc84949984ceb874db712dc3
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-11-25 19:45:36 +0330

    🐛 api fixed

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -31,7 +31,7 @@ export default function Signup() {
 			return;
 		}
 		try {
-			const res = await fetch(`${BASE_URL}/Subscribers/create`, {
+			const res = await fetch(`${BASE_URL}/Subscribers/subscrib/`, {
 				method: "POST",
 				headers: { "Content-Type": "application/json" },
 				body: JSON.stringify({
