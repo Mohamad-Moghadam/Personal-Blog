67d28faef486d17b6d882689895952a206a7427d
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-11-26 20:57:16 +0330

    🐛 blog detail fixed

--- src/app/blog/detail/[id]/page.tsx
+++ src/app/blog/detail/[id]/page.tsx
@@ -1,11 +1,13 @@
-"use client";
-
-import { useParams } from "next/navigation";
 import BlogDetailClient from "./BlogDetailClient";
 
-export default function BlogDetailPage() {
-	const params = useParams();
+interface PageProps {
+	params: { id: string | string[] };
+}
+
+export default function BlogDetailPage({ params }: PageProps) {
 	if (!params?.id) return <p className="p-6 text-red-500">Post ID missing</p>;
 
-	return <BlogDetailClient postId={params.id} />;
+	const postId = Array.isArray(params.id) ? params.id[0] : params.id;
+
+	return <BlogDetailClient postId={postId} />;
 }
