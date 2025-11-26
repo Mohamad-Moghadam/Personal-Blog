018dd1fb44b8fdbd9109bedb91702d40e072c000
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-11-26 21:35:19 +0330

    🐛 blog detail fixed

--- src/app/blog/detail/[id]/BlogDetailClient.tsx
+++ src/app/blog/detail/[id]/BlogDetailClient.tsx
@@ -1,3 +1,4 @@
+// src/app/blog/[id]/BlogDetailClient.tsx
 "use client";
 
 import { useEffect, useState } from "react";
--- src/app/blog/detail/[id]/page.tsx
+++ src/app/blog/detail/[id]/page.tsx
@@ -1,11 +1,16 @@
-"use client";
+// src/app/blog/[id]/page.tsx
 
 import BlogDetailClient from "./BlogDetailClient";
 
-export default function BlogDetailPage({ params }: { params: { id: string } }) {
+export default function BlogDetailPage({
+	params,
+}: {
+	params: { id: string | string[] };
+}) {
 	const postId = Array.isArray(params.id) ? params.id[0] : params.id;
 
 	if (!postId) return <p className="p-6 text-red-500">Post ID missing</p>;
 
+	// Render client component
 	return <BlogDetailClient postId={postId} />;
 }
