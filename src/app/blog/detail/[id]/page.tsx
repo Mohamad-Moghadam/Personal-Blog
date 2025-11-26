ead288b816e52fe7bc28aa8ffa08aaf93dfa323c
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-11-26 21:33:36 +0330

    🐛 blog detail fixed

--- src/app/blog/detail/[id]/page.tsx
+++ src/app/blog/detail/[id]/page.tsx
@@ -2,12 +2,8 @@
 
 import BlogDetailClient from "./BlogDetailClient";
 
-interface Props {
-	params: { id: string | string[] };
-}
-
-export default function BlogDetailPage({ params }: Props) {
-	const postId = Array.isArray(params?.id) ? params.id[0] : params?.id;
+export default function BlogDetailPage({ params }: { params: { id: string } }) {
+	const postId = Array.isArray(params.id) ? params.id[0] : params.id;
 
 	if (!postId) return <p className="p-6 text-red-500">Post ID missing</p>;
 
