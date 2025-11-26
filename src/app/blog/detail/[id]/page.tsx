dc9417a1b76bc603f8309e187aeb07cce8afc1e5
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-11-26 20:49:14 +0330

    🐛 blog detail fixed

--- src/app/blog/detail/[id]/page.tsx
+++ src/app/blog/detail/[id]/page.tsx
@@ -1,11 +1,5 @@
 import BlogDetailClient from "./BlogDetailClient";
 
-interface Props {
-	params: {
-		id: string;
-	};
-}
-
-export default function BlogDetailPage({ params }: Props) {
+export default function BlogDetailPage({ params }: { params: { id: string } }) {
 	return <BlogDetailClient postId={params.id} />;
 }
