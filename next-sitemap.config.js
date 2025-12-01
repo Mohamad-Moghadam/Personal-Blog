aedb3a3b4f54d59f0e3073cac81e619dac6880d6
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-12-01 17:40:11 +0330

    🎉 sitemap added

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.example.com', // replace with your website URL
+  generateRobotsTxt: true, // optional
+  sitemapSize: 7000,       // optional
+}
