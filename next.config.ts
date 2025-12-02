dae3cf48d631b450250766efd0d065afa3db6096
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-12-02 21:25:15 +0330

    🐛 added permission to backend to send photo

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     images: {
-    domains: ["newsdata.io"],
+    domains: ["newsdata.io", "api.msoodmandm.ir"],
   },
 };
 
